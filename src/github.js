968c3ad08d9676b87341bf6eb27e81f3423e5a01
Matthieu Gicquel <matthieugdev@gmail.com>
2020-01-26 19:54:46 +0100

    Github ! (only the most important shortcuts for now)

--- src/github.js
+++ src/github.js
@@ -10,11 +10,24 @@ init_lts([
   { selector: 'header [data-hotkey="g n"]', shortcut: "g n", placement: "bottom", comment: "Notifications" },
 
   /* Repository */
-  { selector: 'nav [data-hotkey="g c"]', shortcut: "g c", comment: "Code" },
-  { selector: 'nav [data-hotkey="g i"]', shortcut: "g i", comment: "Issues"},
-  { selector: 'nav [data-hotkey="g p"]', shortcut: "g p", comment: "Pull requests"},
-  { selector: 'nav [data-hotkey="g w"]', shortcut: "g w", comment: "Wiki" },
-  { selector: 'nav [data-hotkey="g b"]', shortcut: "g b", comment: "Projects" },
+  { selector: '.reponav [data-hotkey="g c"]', shortcut: "g c", comment: "Code" },
+  { selector: '.reponav [data-hotkey="g i"]', shortcut: "g i", comment: "Issues"},
+  { selector: '.reponav [data-hotkey="g p"]', shortcut: "g p", comment: "Pull requests"},
+  { selector: '.reponav [data-hotkey="g w"]', shortcut: "g w", comment: "Wiki" },
+  { selector: '.reponav [data-hotkey="g b"]', shortcut: "g b", comment: "Projects" },
   { selector: '[data-hotkey="w"]', shortcut: "w", comment: "Branches/tags" },
-  { selector: '[data-hotkey="t"]', shortcut: "t", comment: "Find file" }
+  { selector: '[data-hotkey="t"]', shortcut: "t", comment: "Find file" },
+
+  /* File view */
+  { selector: '[data-hotkey="b"]', shortcut: "b", comment: "Blame"},
+
+  /* Issues and PR list views */
+  { selector: '.btn[data-hotkey="c"]', shortcut: "c", comment: "Create new"},
+  { selector: '[data-hotkey="u"]', shortcut: "u", comment: "Filter by author"},
+  { selector: '[data-hotkey="l"]', shortcut: "l", comment: "Filter by label"},
+  { selector: '[data-hotkey="m"]', shortcut: "l", comment: "Filter by milestone"},
+  { selector: '[data-hotkey="a"]', shortcut: "l", comment: "Filter by assignee"},
+
+  /* PR view */
+  /* The shortcuts don't seem to be working currently */
 ]);
