67b05d19ec37a31a6046199895a9605ed95a9973
Matthieu Gicquel <matthieugdev@gmail.com>
2020-03-27 18:35:15 +0100

    Small improvements to github shortcuts

--- src/github.ts
+++ src/github.ts
@@ -12,12 +12,12 @@ init_lts([
   // Disabled until made "smart" (it gets overriden by repo keys)
   // { selector: 'header [data-hotkey="g p"]', keys: "g p", placement: "bottom", comment: "Pull requests" },
   // { selector: 'header [data-hotkey="g i"]', keys: "g i", placement: "bottom", comment: "Issues" },
-  { selector: 'header [data-hotkey="g n"]', keys: "g n", placement: "bottom", comment: "Notifications" },
+  { selector: 'header [data-hotkey="g n"]', keys: "g n", placement: "left", comment: "Notifications" },
 
   /* Repository */
   { selector: '.reponav [data-hotkey="g c"]', keys: "g c", comment: "Code" },
-  { selector: '.reponav [data-hotkey="g i"]', keys: "g i", comment: "Issues"},
-  { selector: '.reponav [data-hotkey="g p"]', keys: "g p", comment: "Pull requests"},
+  { selector: '.reponav [data-hotkey="g i"]', keys: "g i", comment: "Issues" },
+  { selector: '.reponav [data-hotkey="g p"]', keys: "g p", comment: "Pull requests" },
   { selector: '.reponav [data-hotkey="g b"]', keys: "g b", comment: "Projects" },
   { selector: '.reponav [data-hotkey="g w"][data-selected-links^="repo_wiki"]', keys: "g w", comment: "Wiki" },
 
@@ -26,18 +26,17 @@ init_lts([
   /* Source code browsing */
   { selector: '[data-hotkey="t"]', keys: "t", comment: "Find file" },
   { selector: '[data-hotkey="w"]', keys: "w", comment: "Branches/tags" },
-  { selector: '[data-hotkey="b"]', keys: "b", comment: "Blame"},
+  { selector: '[data-hotkey="b"]', keys: "b", comment: "Blame" },
   // These shortcuts have no button
   // TODO: add a way to show shortcuts that are not linked to a UI element
   // { selector: '', keys: "l", comment: "Jump to line" },
   // { selector: '', keys: "y", comment: "Expand url to canonical form" },
   // { selector: '', keys: "i", comment: "Show/hide comment on diff" },
 
-  /* TODO: Comments */
-  { selector: '.composer button[type="submit"]', keys: "meta ↩", placement: "bottom", usable: "whenTyping", comment: "Submit issue"},
-  { selector: '.js-new-comment-form button[type="submit"]', keys: "ctrl ↩", placement: "bottom", usable: "whenTyping", comment: "Submit comment"},
-  { selector: '.tabnav-tabs', keys: "meta ⇧ p", placement: "left", comment: "Switch between edit & preview"},
-
+  /* Issues, PRs & Comments composers */
+  { selector: '.composer button[type="submit"]', keys: "meta ↩", placement: "bottom", usable: "whenTyping", comment: "Submit issue" },
+  { selector: '.js-new-comment-form button[type="submit"]', keys: "ctrl ↩", placement: "bottom", usable: "whenTyping", comment: "Submit comment" },
+  { selector: '.tabnav-tabs', keys: "meta ⇧ p", placement: "left", comment: "Switch between edit & preview" },
 
   /* Issue and pull request lists */
   { selector: '.btn[data-hotkey="c"]', keys: "c", comment: "Create new" },
@@ -48,11 +47,8 @@ init_lts([
   { selector: '[data-hotkey="a"]', keys: "a", comment: "Filter by assignee" },
   { selector: '.js-navigation-item.navigation-focus [type="checkbox"]', keys: "x", placement: "left", comment: "Check item" },
   { selector: '.js-navigation-item.navigation-focus', keys: "↓j ↑k ↩", placement: "inside", comment: "List navigation" },
-
-  // TODO : No way to explain this one for now
+  // TODO: No way to explain this one for now
   // { selector: '', keys: "alt + click", comment: "Exclude label"},
-
-  /* TODO: Issues and pull requests */
 
   /* TODO: Changes in pull requests - These shortcuts don't seem to be working */
   /* TODO: Project boards */
